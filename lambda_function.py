749d44cf06eb11848eac28a23af0dcfa913a34ea
Mark Williams <mark.williams@diamond.ac.uk>
2023-02-08 15:45:33 +0000

    Add methods to get tomorrows solar forecast from solcast

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timedelta
 
 
 def get_headers(apikey):
@@ -156,6 +157,25 @@ def set_AC_charge_limit(headers, value, inverter_id=None):
     if 'data' in r.json() and 'success' in r.json()['data']:
         return r.json()['data']['success']
 
+
+def get_solcast_forecast(solcast_key, solcast_site):
+    solcast_headers = get_headers(solcast_key)
+    url = 'https://api.solcast.com.au/rooftop_sites/'+solcast_site+'/forecasts?format=json'
+    r = requests.get(url, headers=solcast_headers)
+    if 'forecasts' in r.json():
+        return r.json()['forecasts']
+    return []
+
+
+def get_tomorrows_forecast_total(solcast_key, solcast_site):
+    forecast = get_solcast_forecast(solcast_key, solcast_site)
+    total = 0
+    tomorrow = datetime.now()+timedelta(days=1)
+    for i in forecast:
+        end = datetime.strptime(i['period_end'], '%Y-%m-%dT%H:%M:%S.%f0Z')
+        if end.date() == tomorrow.date():
+            total += i['pv_estimate'] / 2
+    return total
 
 
 def on_intent(event, headers):
