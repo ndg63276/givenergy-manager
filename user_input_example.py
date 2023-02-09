5a2156eb5ba17d860d328e59e7d53bd87f4a833f
Mark Williams <mark.williams@diamond.ac.uk>
2023-02-09 12:51:10 +0000

    Add a place for apikeys and user settings

---
+++ user_input_example.py
@@ -0,0 +1,9 @@
+givenergy_key = ""
+solcast_key = ""
+solcast_site = ""
+
+very_sunny_day = 20  # anything above this (in kWh) is considered very sunny
+very_sunny_day_charge = 70  # charge to this percentage for any very sunny day
+
+not_sunny_day = 10  # anything below this (in kWh) is considered not sunny
+not_sunny_day_charge = 100  # charge to this percentage for any not sunny day
