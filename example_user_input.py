e080da89911202a0d91145358432180b9c3a0e98
ndg63276 <24956497+ndg63276@users.noreply.github.com>
2023-02-11 17:29:49 +0000

    Update example with Tapo fields

--- example_user_input.py
+++ example_user_input.py
@@ -15,3 +15,11 @@ not_sunny_day_charge = 100  # charge to this percentage for any not sunny day
 time_to_set_max_charge = "20:00"  # 24hr clock representation of the time to set the max charge
 
 times_to_check_errors = [2,32]  # minutes past the hour to check for errors
+
+tapo_enable_if_battery_full = False  # set True to turn on a tapo smart plug when battery gets full
+battery_full_levels = [
+    ["00:00", 100],
+]
+tapo_ip = ""  # Tapo plug has to be on the local network, set the IP here
+tapo_username = ""  # Tapo login details
+tapo_password = ""
