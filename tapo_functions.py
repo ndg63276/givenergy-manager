5427d9da59bc25b823533d5083354dd9c91ecf19
ndg63276 <24956497+ndg63276@users.noreply.github.com>
2023-02-13 11:45:57 +0000

    Standardise formatting

--- tapo_functions.py
+++ tapo_functions.py
@@ -2,20 +2,20 @@ from PyP100 import PyP100
 from user_input import tapo_ip, tapo_username, tapo_password
 
 
-def getTapoPlug():
+def get_tapo_device():
     p100 = PyP100.P100(tapo_ip, tapo_username, tapo_password)
     p100.handshake()
     p100.login()
     return p100
 
 
-def tapoPlugIsOn(p100):
+def tapo_device_is_on(p100):
     info = p100.getDeviceInfo()
     return info["result"]["device_on"]
 
 
-def switchTapoPlug(p100, turnOn):
-    if turnOn:
+def switch_tapo_device(p100, turn_on):
+    if turn_on:
         p100.turnOn()
     else:
         p100.turnOff()
