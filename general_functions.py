51b80e152f267cb1178d82cc2a320e1f682022c9
ndg63276 <24956497+ndg63276@users.noreply.github.com>
2023-03-01 20:10:26 +0000

    Add path to json file

--- general_functions.py
+++ general_functions.py
@@ -1,10 +1,11 @@
+import os
 import json
 import smtplib
 from email.mime.text import MIMEText
 
 
 def read_json():
-    with open("user_input.json", "r") as f:
+    with open(os.path.join(os.path.dirname(__file__), "user_input.json"), "r") as f:
         j = json.load(f)
     return j
 
